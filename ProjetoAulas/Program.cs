4ca451ffbe17c6f2e0f802916c16774b5cfe911c
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2023-01-01 21:47:18 -0300

    Trabalhando com Variaveis

---
+++ ProjetoAulas/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Primeiro Programa");
+
+//Trabalhando com Variáveis.
+
+int idade = 30;
+Console.WriteLine(idade);
+
+string nome = "Lucivaldo";
+Console.WriteLine(nome);
+
+decimal valor = 200.99m;
+Console.WriteLine(valor);
+double valorDouble = 200.99;
+Console.WriteLine(valorDouble);
+float valorFloat = 200.99f;
+Console.WriteLine(valorFloat);
+
+var idadeNova = 34;
+Console.WriteLine(idadeNova);
+
+char flag = 'C';
+Console.WriteLine(flag);
+bool ativo = true;
+Console.WriteLine(ativo);
