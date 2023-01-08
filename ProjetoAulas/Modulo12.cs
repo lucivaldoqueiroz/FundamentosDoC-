607933f300061bfc5d3f5b52647ef2d87506118e
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2023-01-07 23:20:56 -0300

    Tratando uma exception

--- ProjetoAulas/Modulo12.cs
+++ ProjetoAulas/Modulo12.cs
@@ -6,10 +6,21 @@ public class TrabalhandoComException
     {
        while(true)
        {
-         Console.Write("Informe um numero: ");
+        try{
+             Console.Write("Informe um numero: ");
         var numero = Console.ReadLine();
         var resultado = 500 / int.Parse(numero);
         Console.WriteLine("Resultado: " + resultado);
+        }
+        catch(DivideByZeroException exception){
+            Console.WriteLine("Ocorreu um erro de divisão ! " + exception.Message);
+            Console.WriteLine("Stack " + exception.StackTrace);
+        }
+        catch(Exception exception){
+            Console.WriteLine("Ocorreu um erro ! " + exception.Message);
+            Console.WriteLine("Stack " + exception.StackTrace);
+        }
+        
        }
 
     }
