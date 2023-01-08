19b7e4536b79bf4dcd31cec5c40715171a9f95aa
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2023-01-08 13:27:38 -0300

    Criando projeto App Clientes

---
+++ AppClientes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AppClientes;
+class Program
+{
+    static void Main(string[] args)
+    {
+        while(true)
+        {
+            Menu();
+            Console.ReadKey();
+        }
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        
+        Console.WriteLine("Cadastro de Clientes");
+        Console.WriteLine("--------------------");
+        Console.WriteLine("1 - Cadastrar Cliente");
+        Console.WriteLine("2 - Exibir Clientes");
+        Console.WriteLine("3 - Exibir Clientes");
+        Console.WriteLine("4 - Excluir Clientes");
+        Console.WriteLine("5 - Sair");
+        Console.WriteLine("--------------------");
+    }
+}
